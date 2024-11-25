13212d048d94842cbeb6923fabe4339c835588a9
Jypaaaa <amk1006156@student.hamk.fi>
2024-11-25 17:24:20 +0200

    Korjattu muutamia loogisuus virheitä. /JM

--- src/Arvauspeli.java
+++ src/Arvauspeli.java
@@ -8,26 +8,28 @@ public class Arvauspeli {
         Random rand = new Random();
         String arvo = "k";
  
-        int eka = rand.nextInt(10);
-        int toka = rand.nextInt(10);
-        int kolmas = rand.nextInt(10);
+        int Eka = rand.nextInt(10);
+        int Toka = rand.nextInt(10);
+        int Kolmas = rand.nextInt(10);
+
         int raha;
 
 
         while (true) {
             System.out.println("Kuinka paljon rahaa haluat käyttää?");
-            raha = in.nextLine();
+            raha = in.nextInt();
+
             raha--;
 
-            if (raha.equals(0)) {
+            if (raha <= 0) {
                 System.out.println("rahat loppui");
                 break;
             }
 
-            if (raha.equals(1,2,3,4,5)) {
+            if (raha >= 1 && raha <= 5) {
                 System.out.println("Congratulations!");
                 System.out.println(".");
-                break; 
+                break;    
             } 
         }
     }
